b081b3f31f0a0bb3ce81dd451b5edf99b244e406
Boris <office@bm-it.dev>
2024-03-12 00:08:03 +0300

    added some conditions

--- main.go
+++ main.go
@@ -1,49 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	operatorSlice := [4]string{"+", "-", "*", "/"}
-	inputValue1 := 0
-	inputValue2 := 0
-	inputOperator := ""
-	isOperatorFound := false
-	result := 0
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Введите выражение (например, 5 + 3):")
+	scanner.Scan()
+	inputString := scanner.Text()
 
-	fmt.Println("Введите первое число: ")
-	fmt.Scanln(&inputValue1)
-	fmt.Println("Зафиксировано первое число: ", inputValue1)
-	fmt.Println("Введите оператор: ")
-	fmt.Scanln(&inputOperator)
-	fmt.Println("Введите второе число: ")
-	fmt.Scanln(&inputValue2)
-	fmt.Println("Зафиксировано второе число: ", inputValue2)
-
-	for _, operator := range operatorSlice {
-		if operator == inputOperator {
-			isOperatorFound = true
-			break
-		}
+	valuesArray := strings.Fields(inputString)
+	if len(valuesArray) != 3 {
+		fmt.Println("Неверный формат ввода.")
+		return
 	}
 
-	if inputOperator != "" && isOperatorFound == true {
-		switch inputOperator {
-		case "+":
-			result = inputValue1 + inputValue2
-		case "-":
-			result = inputValue1 - inputValue2
-		case "*":
-			result = inputValue1 * inputValue2
-		case "/":
-			if inputValue1 != 0 && inputValue2 != 0 {
-				result = inputValue1 / inputValue2
-			} else {
-				fmt.Println("Одно из значений = 0. На ноль делить нельзя!")
-				return
-			}
-		}
+	inputValue1, err1 := strconv.Atoi(valuesArray[0])
+	inputOperator := valuesArray[1]
+	inputValue2, err2 := strconv.Atoi(valuesArray[2])
 
+	if err1 != nil || err2 != nil || inputValue1 < 1 || inputValue1 > 10 || inputValue2 < 1 || inputValue2 > 10 {
+		fmt.Println("Ошибка: числа должны быть в диапазоне от 1 до 10.")
+		return
 	}
 
-	fmt.Println("Ваш результат: ", result)
+	var result int
+	switch inputOperator {
+	case "+":
+		result = inputValue1 + inputValue2
+	case "-":
+		result = inputValue1 - inputValue2
+	case "*":
+		result = inputValue1 * inputValue2
+	case "/":
+		if inputValue2 == 0 {
+			fmt.Println("На ноль делить нельзя!")
+			return
+		} else {
+			result = inputValue1 / inputValue2
+		}
+	default:
+		fmt.Println("Неверный оператор.")
+		return
+	}
+
+	fmt.Printf("Ваш результат: %d\n", result)
 }
